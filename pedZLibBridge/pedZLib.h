5ddee53a3681461e5ae4c88bfded471e025ffde2
Pedro <pedro.alvarez@msab.com>
2022-11-15 21:48:44 +0100

    remove zlib

--- pedZLibBridge/pedZLib.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <windows.h>
-#include <string>
-
-static const size_t MaxInflateBuffer = 128 * 1024;
-
-std::string Compress(const char* data, size_t dataLen, size_t BufferSize, const std::string& CompressionDict);
-std::string Decompress(const char* data, size_t dataLen, size_t BufferSize, const std::string& CompressionDict);
